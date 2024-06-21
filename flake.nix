f3301bed9a8d91d46fe755798e7d8f72d45239e5
Nathan Lloyd Ward <wardnath.alt@gmail.com>
2024-06-21 13:11:07 -0400

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+        url = "github:oxalica/rust-overlay";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      inputs = with pkgs; [
+            pkg-config
+            rust-bin.stable.latest.default
+          ];
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "code2prompt";
+          version = "0.0.1";
+
+          # src = ./.;
+          src = pkgs.lib.cleanSource ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+
+          buildInputs = inputs;
+          nativeBuildInputs = inputs;
+
+          buildPhase = ''
+                        cargo build --release
+          '';
+          installPhase = ''
+                   mkdir -p $out/bin
+                   cp code2prompt $out/bin/
+                  '';
+        };
+        devShells.default = pkgs.mkShell {
+          packages = inputs;
+        };
+      });
+}
+
