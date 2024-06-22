0bc7b0214f9941f461ded3420b6ecff762d14d7e
Nathan Lloyd Ward <wardnath.alt@gmail.com>
2024-06-21 20:28:12 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,46 +3,46 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
-    	url = "github:oxalica/rust-overlay";
-	inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  
   outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs { inherit system overlays; };
       inputs = with pkgs; [
-	    openssl
-            pkg-config
-            cacert
-            cargo-make
-            trunk
-	    rust-bin.stable.latest.default
-          ];
-      in {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "code2prompt";
-          version = "0.0.1";
+        openssl
+        pkg-config
+        cacert
+        cargo-make
+        trunk
+        rust-bin.stable.latest.default
+      ];
+    in {
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "code2prompt";
+        version = "0.0.1";
+        
+        src = pkgs.lib.cleanSource ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        
+        nativeBuildInputs = inputs;
 
-          # src = ./.;
-          src = pkgs.lib.cleanSource ./.;
+        buildPhase = ''
+          cargo build --release
+        '';
 
-          cargoLock.lockFile = ./Cargo.lock;
-          
-	  #buildInputs = inputs;
-	  nativeBuildInputs = inputs;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/release/code2prompt $out/bin/
+        '';
+      };
 
-          buildPhase = ''
-			cargo build --release 
-          '';
-          installPhase = ''
-		   mkdir -p $out/bin
-                   cp code2prompt $out/bin/
-		  '';
-        };
-        devShells.default = pkgs.mkShell {
-          packages = inputs;
-        };
-      });
+      devShells.default = pkgs.mkShell {
+        packages = inputs;
+      };
+    });
 }
